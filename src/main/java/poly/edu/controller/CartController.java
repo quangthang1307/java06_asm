24100f89fdd7dba122555574bf08b8e19ae58ca2
quangthang1307 <tn0429221@gmail.com>
2023-11-07 01:27:41 +0700

    merge master

--- src/main/java/poly/edu/controller/CartController.java
+++ src/main/java/poly/edu/controller/CartController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CartController {
 	@RequestMapping("/cart")
 	public String showCart() {
+		System.out.println("them cai nua ne");
 		return "user/cart";
 	}
 }
