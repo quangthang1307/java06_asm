16346c4f0cd347d54ecbc00a8cc686e2828de926
quangthang1307 <tn0429221@gmail.com>
2023-11-07 10:07:17 +0700

    add sout

--- src/main/java/poly/edu/controller/CartController.java
+++ src/main/java/poly/edu/controller/CartController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CartController {
 	@RequestMapping("/cart")
 	public String showCart() {
+		System.out.println("ád");
 		return "user/cart";
 	}
 }
